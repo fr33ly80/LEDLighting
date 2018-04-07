a9122811372c2e3aece6c49802d0eefe381dccc8
fr33ly80 <freely.brendan@gmail.com>
2018-04-07 13:22:14 -0400

    Initial Commit

---
+++ LEDController.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  7 12:35:28 2018
+
+@author: Brendan
+"""
+
+from ControllerInterface import ControllerInterface as Controller
+
+class LEDController(Controller):
+    def __init__(self):
+        pass
---
+++ __init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  7 12:03:13 2018
+
+@author: Brendan
+"""
+
